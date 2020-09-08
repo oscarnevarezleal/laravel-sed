46228123d4d27b98a150d319ed628568f610f5b9
Oscar Nevarez <fu.wwire@gmail.com>
2020-09-08 16:19:05 -0500

    Update Command to Match laravel class signature

--- cli/php/src/Console/Commands/ConfigEditCommand.php
+++ cli/php/src/Console/Commands/ConfigEditCommand.php
@@ -3,11 +3,37 @@
 namespace Laraboot\Console\Commands;
 
 use Laraboot\Commands\EditConfigFileCommand;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class ConfigEditCommand extends EditConfigFileCommand
 {
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $laravel;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'larased:config-edit';
+
+    /**
+     * The console command help text.
+     *
+     * @var string|null
+     */
+    protected $help;
+
+    /**
+     * Indicates whether the command should be shown in the Artisan command list.
+     *
+     * @var bool
+     */
+    protected $hidden = false;
+
     /**
      * The name and signature of the console command.
      *
@@ -22,33 +48,19 @@ class ConfigEditCommand extends EditConfigFileCommand
      */
     protected $description = 'Edit a configuration value and persist the result in file';
 
+    /**
+     * @return \Symfony\Component\Console\Input\InputOption[]
+     */
     protected function getOptions()
     {
         return $this->getDefinition()->getOptions();
     }
 
+    /**
+     * @return \Symfony\Component\Console\Input\InputArgument[]
+     */
     protected function getArguments()
     {
         return $this->getDefinition()->getArguments();
-    }
-
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return int
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        return (int)parent::execute($input, $output);
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        return null;
     }
 }
