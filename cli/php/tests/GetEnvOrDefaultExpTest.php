6d99e59068bb4d8487f44d040c50e4a9827bc2bb
Oscar Nevarez <fu.wire@gmail.com>
2020-11-19 13:00:08 -0600

    Test refactored.

--- cli/php/tests/GetEnvOrDefaultExpTest.php
+++ cli/php/tests/GetEnvOrDefaultExpTest.php
@@ -6,23 +6,36 @@ use PHPUnit\Framework\TestCase;
 
 class GetEnvOrDefaultExpTest extends TestCase
 {
-    public function testGetEnvOrDefaultExp()
+    /**
+     * Assert that given a list of environment variables names,
+     * the result expression contains a nested call to _env_ function,
+     * having the deepest call a default value.
+     * E.g. env('ORGANIZATION_NAME', env('PROJECT_NAME', env('APP_NAME', 'MyApp')))
+     */
+    public function testChainOfEnvsOrDefaultExp()
     {
+        $traverser = new NodeTraverser;
+        $prettyPrinter = new PrettyPrinter\Standard;
+
         $envList = [
             'ORGANIZATION_NAME',
             'PROJECT_NAME',
             'APP_NAME'
         ];
 
-        $stmts = GetEnvOrDefaultExp::chainOfEnvCallsWithDefault('key', $envList, 'MyApp');
+        $default = 'MyApp';
 
-        $traverser = new NodeTraverser;
-        $prettyPrinter = new PrettyPrinter\Standard;
+        $stmts = GetEnvOrDefaultExp::chainOfEnvCallsWithDefault('key', $envList, $default);
 
         $ast = $traverser->traverse([$stmts]);
-
         $print = $prettyPrinter->prettyPrintFile($ast);
 
-        echo $print;
+        $assertLine = sprintf("env('%s', env('%s', env('%s', '%s')))",
+            $envList[0],
+            $envList[1],
+            $envList[2],
+            $default);
+
+        $this->assertStringContainsString($assertLine, $print);
     }
 }
