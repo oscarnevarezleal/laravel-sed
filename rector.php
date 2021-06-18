35251b255503fdd6f2ca5ef1d7db8b1a2dd0af34
Oscar Nevarez <fu.wire@gmail.com>
2021-06-17 20:04:59 -0500

    chore(ci-rector): Include rector

--- rector.php
+++ rector.php
@@ -1,13 +1,10 @@
 <?php
-// rector.php
 
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
@@ -16,14 +13,16 @@ use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php80\Rector\If_\NullsafeOperatorRector;
 use Rector\Php80\Rector\NotIdentical\StrContainsRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-//use Utils\Rector\MyFirstRector;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::BOOTSTRAP_FILES, [
+        __DIR__ . '/vendor/autoload.php',
+    ]);
+
+    $containerConfigurator->import(SetList::CODE_QUALITY);
 
     // paths to refactor; solid alternative to CLI arguments
     $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
@@ -36,7 +35,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     // here we can define, what sets of rules will be applied
     $parameters->set(Option::SETS, [SetList::CODE_QUALITY]);
-
     // auto import fully qualified class names? [default: false]
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     // skip root namespace classes, like \DateTime or \Exception [default: true]
@@ -44,6 +42,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // skip classes used in PHP DocBlocks, like in /** @var \Some\Class */ [default: true]
     $parameters->set(Option::IMPORT_DOC_BLOCKS, true);
 
+    // Services
     $services = $containerConfigurator->services();
     $services->set(TypedPropertyRector::class);
     $services->set(ClosureToArrowFunctionRector::class);
